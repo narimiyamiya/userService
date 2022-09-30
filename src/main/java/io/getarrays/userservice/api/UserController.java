a51c1fcaa60a1e397448fa54424eea3bfa977385
Chloe <navymeester@gmail.com>
2022-09-30 18:18:33 +0800

    20220930

--- src/main/java/io/getarrays/userservice/api/UserController.java
+++ src/main/java/io/getarrays/userservice/api/UserController.java
@@ -1,18 +1,20 @@
 package io.getarrays.userservice.api;
 
+import io.getarrays.userservice.domain.Role;
 import io.getarrays.userservice.domain.User;
 import io.getarrays.userservice.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
     private  final UserService userService;
 
@@ -21,4 +23,28 @@ public class UserController {
         return ResponseEntity.ok().body(userService.getUser());
     }
 
+    @PostMapping("/user/save")
+    public ResponseEntity<User>saveUser(@RequestBody User user) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role>saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addToUser")
+    public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToUser(form.getUserName(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+    @Data
+     static class RoleToUserForm {
+        private  String userName;
+        private  String roleName;
+    }
 }
--- src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
+++ src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
@@ -6,16 +6,38 @@ import io.getarrays.userservice.repo.RoleRepo;
 import io.getarrays.userservice.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUserName(username);
+        if (user == null){
+            log.error("查無此帳號");
+            throw new UsernameNotFoundException("查無此帳號");
+        }else{
+            log.info("帳號已被搜尋到:{}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), authorities);
+    }
 
     @Override
     public User saveUser(User user) {
@@ -48,4 +70,6 @@ public class UserServiceImpl implements UserService{
         log.info("Fetching all users ()");
         return userRepo.findAll();
     }
+
+
 }
